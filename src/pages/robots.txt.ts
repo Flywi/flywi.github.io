62da8e2766ca64f532f370f1e5857db623ee5b37
WANGJINXIN <wangjinxin@gigacloudtech.com>
2026-06-05 16:47:21 +0800

    Deploy built Astro site

--- src/pages/robots.txt.ts
+++
@@ -1,6 +0,0 @@
-export function GET() {
-  return new Response(
-    ['User-agent: *', 'Allow: /', 'Disallow: /.git/', 'Sitemap: https://jrain.cn/sitemap-index.xml', ''].join('\n'),
-    { headers: { 'Content-Type': 'text/plain; charset=utf-8' } },
-  );
-}
